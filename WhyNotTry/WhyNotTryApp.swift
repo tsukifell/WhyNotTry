9d7bc4ca636f3ebae6de54e846e59dc7a584f21a
Calvin Saputra <calvinsaputra217@gmail.com>
2024-06-08 10:07:33 +0700

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by admin on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
